ca4c65def5f19754654977f1b83a701f67161eb2
James Petry <gavilancomun@gmail.com>
2015-02-19 19:49:12 +0000

    first

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -288,10 +288,11 @@ produce f x = x :. (produce f (f x))
 -- prop> let types = x :: List Int in notReverse x ++ notReverse y == notReverse (y ++ x)
 --
 -- prop> let types = x :: Int in notReverse (x :. Nil) == x :. Nil
-notReverse :: List a -> List a
+--notReverse :: List a -> List a
+notReverse :: List Int -> List Int
 notReverse Nil = Nil
 notReverse (x :. Nil) = x :. Nil
-notReverse (x :. xs) = (x :. xs)
+notReverse (x :. xs) = take (length (x :. xs)) (produce id 0)
 
 ---- End of list exercises
 
